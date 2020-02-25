dadabfcf6aba4f650e2b5b4f7e05fb6ff5e1b52a
ExT9453 <61157216+ExT9453@users.noreply.github.com>
2020-02-26 04:19:12 +0900

    First

---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class PlayerJump : MonoBehaviour
+{
+
+
+
+    RaycastHit Hit;
+
+  
+    Vector3 startPos;
+    Vector3 destPos;
+    private bool isJumping;
+    float timer;
+    float vx;
+    float vy;
+    float vz;
+    float sx;
+    float sy;
+    float sz;
+    void Start()
+    {
+        isJumping = false;
+    
+
+    }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+
+
+        if (Input.GetMouseButton(0))
+        {
+            if (isJumping == false)
+            {
+                // 마우스 왼쪽 클릭시
+
+                Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out Hit);
+        
+                //마우스 포지션으로 빛을 쏴서 그 값을 hit 에 저장
+
+                destPos = Hit.point;
+
+                //hit 값을 Click 저장
+                startPos = transform.position;
+                vx = (destPos.x - startPos.x) / 2f;
+                vz = (destPos.z - startPos.z) / 2f;
+                vy = (destPos.y - startPos.y + 2 * 9.8f) / 2f;
+                isJumping = true;
+                //transform.Translate((Click - transform.position).normalized * MoveSpeed * Time.deltaTime);
+
+
+
+                // 클릭좌표로 이동
+            }
+        }
+        if (isJumping == true)
+        {
+           
+            timer += Time.deltaTime;
+             sx = startPos.x + vx * timer;
+             sy = startPos.y + vy * timer - 0.5f * 9.8f * timer * timer;
+             sz = startPos.z + vz * timer;
+            transform.position = new Vector3(sx, sy, sz);
+         
+        }
+
+    }
+
+    void OnCollisionEnter(Collision ground)
+    {
+        if (ground.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+
+        }
+    }
+    /*
+    void OnCollisionStay(Collision ground)
+    {
+        if (ground.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+
+        }
+    }
+    void OnCollisionExit(Collision ground)
+    {
+        if (ground.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+
+        }
+    }
+    */
+}
+
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public int JumpPower;
+    public int speed;
+    private Rigidbody rigid;
+    private bool IsJumping;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+       IsJumping = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveObject();
+        Jump();
+       
+    }
+    void moveObject()
+
+    {
+
+        float h = Input.GetAxis("Horizontal");
+
+        float v = Input.GetAxis("Vertical");
+
+        
+        transform.Translate(Vector3.right.normalized * speed * Time.smoothDeltaTime * h, Space.World);
+
+        transform.Translate(Vector3.forward.normalized * speed * Time.smoothDeltaTime * v, Space.World);
+
+    }
+
+
+    void Jump()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            if (!IsJumping)
+            {
+                
+                IsJumping = true;
+                rigid.AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+            }
+            
+            else
+            {
+                return;
+            }
+        }
+    }
+
+
+   
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+          
+            IsJumping = false;
+        }
+    }
+
+
+ 
+   
+}
