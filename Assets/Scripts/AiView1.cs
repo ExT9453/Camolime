e32d95d78aedb31c26d218b2e2f0ce15cb55f5fa
Melrabbit0 <67998533+Melrabbit0@users.noreply.github.com>
2020-07-22 19:48:38 +0900

    0722 ai 수정
    .

---
+++ Assets/Scripts/AiView1.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiView : MonoBehaviour
+{
+    [SerializeField] float m_angle = 0f;
+    [SerializeField] float m_distance = 0f;
+    [SerializeField] LayerMask m_layerMask = 0;
+
+    public Transform target;
+    public Vector3 direction;
+    public float velocity;
+    public float accelaration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sight();
+        moveAI();
+
+    }
+
+    public void moveAI()
+    {
+
+        target = GameObject.Find("1").transform;
+        // Player의 위치와 이 객체의 위치를 빼고 단위 벡터화 한다.
+        direction = (target.position - transform.position).normalized;
+        // 가속도 지정 (추후 힘과 질량, 거리 등 계산해서 수정할 것)
+        accelaration = 0.01f;
+        // 초가 아닌 한 프레임으로 가속도 계산하여 속도 증가
+        velocity = (velocity + accelaration * Time.deltaTime);
+        // Player와 객체 간의 거리 계산
+        float distance = Vector3.Distance(target.position, transform.position);
+        // 일정거리 안에 있을 시, 해당 방향으로 무빙
+        if (distance <= 10.0f)
+        {
+            this.transform.position = new Vector3(transform.position.x + (direction.x * velocity),
+                                                    transform.position.y,
+                                                    transform.position.z + (direction.z * velocity)
+                                                    );
+        }
+        // 일정거리 밖에 있을 시, 속도 초기화 
+        else
+        {
+            velocity = 0.0f;
+        }
+
+    }
+
+
+    //void sight()
+    //{
+    //    Collider[] t_cols = Physics.OverlapSphere(transform.position, m_distance, m_layerMask);
+    //    if (t_cols.Length > 0)
+    //    {
+    //        Transform t_tfPlayer = t_cols[0].transform;
+
+    //        Vector3 t_direction = (t_tfPlayer.position - transform.position).normalized;
+    //        float t_angle = Vector3.Angle(t_direction, transform.forward);
+    //        if (t_angle < m_angle * 0.5f)
+    //        {
+    //            if (Physics.Raycast(transform.forward, t_direction, out RaycastHit t_hit, m_distance))
+    //            {
+    //                if (t_hit.transform.name == "Player")
+    //                {
+    //                    transform.position = Vector3.Lerp(transform.position, t_hit.transform.position, 0.02f);
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+}
