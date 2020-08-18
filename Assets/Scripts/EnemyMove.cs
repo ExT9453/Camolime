f6032e1157fa34db6e2100a5c7b618ca6d272834
Dokus780 <62826511+Dokus780@users.noreply.github.com>
2020-08-18 22:09:01 +0900

    200818 스파인 적용
    구글에서 스파인 런타임 검색후 유니티에 임포트 할것!
    
    캐몰라임 스파인 적용
    이동, 냠, 퉤
    
    드론 스파인 적용
    수정 예정 참조
    
    수정 내용 :
    스크립트폴더
     - EnemyAnimInstance
     - PlayerAnimInstance
     - AnimInstance
     - EnemyCharacter
     - PlayerSpineMove
     - Enums
    추가
     - EnemyMove 수정
    
    수정 예정 :
    스파인 애니메이션이 적용된 스크립트를 애니메이터로 바꿀거임
    
    * 기존에 있는 Camolime 에 스파인 적용하려 했지만
    ai 가 제대로 안따라와서 따로 만듬

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,72 @@
+﻿using Spine;
+using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public sealed class EnemyMove : MonoBehaviour
+{
+    #region Inspector
+    [SpineAnimation]
+    public string moveAnimation;
+
+    #endregion
+    private SkeletonAnimation _SkeletonAnimation;
+    private SkeletonRenderer _SkeletonRenderer;
+
+    [Header("드론 이동 속도")]
+    // 이동 속도
+    private float _EnemySpeed = 8.0f;
+    
+    // 좌, 우 끝 좌표
+    public const float _EnemyRightPosition = 8.0f;
+    public const float _EnemyLeftPosition = -8.0f;
+
+    public float _EnemyPosition;
+
+    // 초기 위치
+    private float _EnemyFirstPosition;
+
+    
+
+    private void Awake()
+    {
+        // 초기 위치 설정
+        _EnemyFirstPosition = _EnemyPosition;
+
+        //_SkeletonAnimation.state.SetAnimation(0, "moving", true);
+    }
+
+    private void Update()
+    {
+        // 왼쪽 이동
+        LeftMove();
+
+        //_SkeletonAnimation.timeScale = 1.5f;
+        //
+        //_SkeletonAnimation.loop = true;
+        //
+        //_SkeletonAnimation.AnimationName = "moving";
+    }
+
+    // 한시적으로 왼쪽으로만 움직임
+    private void LeftMove()
+    {
+        // 왼쪽으로 이동
+        transform.Translate(Vector3.left * _EnemySpeed * Time.deltaTime, Space.World);
+
+        // 좌, 우 끝 좌표를 넘어가지 않게끔
+        Vector2 currentPosition = transform.position;
+
+        currentPosition.x = Mathf.Clamp(currentPosition.x, _EnemyLeftPosition, _EnemyRightPosition);
+
+        transform.position = currentPosition;
+    }
+
+    private void ChangeState()
+    {
+    }
+}
