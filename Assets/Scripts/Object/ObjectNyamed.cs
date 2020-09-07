937c9390c33c680f80eddc8b4abae39bccdd6b5d
Dokus780 <62826511+Dokus780@users.noreply.github.com>
2020-09-08 04:46:37 +0900

    0908
    일단 야매

---
+++ Assets/Scripts/Object/ObjectNyamed.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectNyamed : MonoBehaviour
+{
+    [SerializeField] float m_angle = 0f;
+    [SerializeField] float m_distance = 0f;
+
+    Animator animator;
+
+    public Transform target;
+    public Vector3 direction;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Nyamed();
+    }
+
+     public void Nyamed()
+     {
+        target = GameObject.Find("1").transform;
+        // Player와 객체 간의 거리 계산
+        float distance = Vector3.Distance(target.position, transform.position);
+        // 일정거리 안에 있을 시
+        if (distance >= 2.0f)
+        {
+            Debug.Log("냠 불가");
+            animator.SetBool("_Close", false);
+
+            //if(this.direction.x<=0)
+            //{
+            //}
+            //else
+            //{
+            //}
+        }
+        else
+        {
+            Debug.Log("냠 가능");
+            
+            animator.SetBool("_Close", true);
+
+            if (Input.GetKeyDown(KeyCode.J))
+                animator.SetBool("_Nyamed", true);
+        }
+    }
+}
