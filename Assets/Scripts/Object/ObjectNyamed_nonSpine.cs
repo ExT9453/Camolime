bbcd4a5fcc4abf942e03cbc470738aa76cf0f588
Dokus780 <62826511+Dokus780@users.noreply.github.com>
2020-09-08 15:27:18 +0900

    0908 색뺏기 오브젝트 추가
    빨간차, board, box 색 뺏기 구현
    board 색 잃는 스프라이트 적용

---
+++ Assets/Scripts/Object/ObjectNyamed_nonSpine.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectNyamed_nonSpine : MonoBehaviour
+{
+    [SerializeField] float m_angle = 0f;
+    [SerializeField] float m_distance = 0f;
+
+    private bool nyamedCheck;
+
+    public Sprite Original;
+    public Sprite CamolimeTouch;
+    public Sprite CamolimeNyamed;
+    public Transform target;
+    public Vector3 direction;
+
+    void Start()
+    {
+        nyamedCheck = false;
+    }
+
+    void Update()
+    {
+        Nyamed();
+    }
+
+     public void Nyamed()
+     {
+        target = GameObject.Find("1").transform;
+        // Player와 객체 간의 거리 계산
+        float distance = Vector3.Distance(target.position, transform.position);
+        // 일정거리 안에 있을 시
+        if (!nyamedCheck)
+        {
+            if (distance >= 2.0f)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = Original;
+            }
+            else
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = CamolimeTouch;
+                if (Input.GetKeyDown(KeyCode.J))
+                {
+                    nyamedCheck = true;
+                }
+            }
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = CamolimeNyamed;
+        }
+    }
+}
