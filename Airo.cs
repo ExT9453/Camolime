1a2700f1d4cfa1164cd0e485277f7f9c0386dc9f
Melrabbit0 <67998533+Melrabbit0@users.noreply.github.com>
2020-11-13 20:09:10 +0900

    Add files via upload
    pie - flag 따라 이동하도록 수정(pie 오브젝트에 스크립트 넣어주면 작동)
    Airo - flag 따라 이동할때 ai 모션 앞뒤 바뀌도록(스케일 수정 필요)

---
+++ Airo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airo : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    int movementFlag = 2;
+    void Start()
+    {
+        StartCoroutine("ChangeMovement");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject tempObj = null;
+        tempObj = GameObject.Find("movementFlag");
+
+
+        if (movementFlag == 1)
+        {
+            //moveVelocity = Vector3.left;
+            //transform.localScale = new Vector3(3.5f, 3.5f, 3.5f);
+
+
+            // chrRenderer.flipX = false;
+
+            this.transform.rotation = Quaternion.Euler(0, -180, 0);
+        }                                          
+        else if (movementFlag == 2)
+        {
+            //moveVelocity = Vector3.right;
+
+            this.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    IEnumerator ChangeMovement()
+    {
+        if (movementFlag == 1)
+        {
+            movementFlag = 2;
+        }
+        else if (movementFlag == 2)
+        {
+            movementFlag = 1;
+        }
+        /*movementFlag =1;*/ /*Random.Range(0, 3);*/
+
+        yield return new WaitForSeconds(10f);
+
+        StartCoroutine("ChangeMovement");
+    }
+}
+
