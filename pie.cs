59a6ffa77a3f34e2873b5fe452e00d27749cb627
Melrabbit0 <67998533+Melrabbit0@users.noreply.github.com>
2020-11-16 16:36:16 +0900

    AI 충돌 및 자동 움직이기
    Flag를 맵 위에 두고 그 Flag에 충돌하면 다음 flag를 찾아 반복하는 방식.

--- pie.cs
+++ pie.cs
@@ -9,25 +9,27 @@ public class pie : MonoBehaviour
     Vector3 chrPos;
 
     public Transform target;
+    public Transform target2;
+    public Transform target3;
     private SpriteRenderer chrRenderer;
     public Vector3 direction;
     public Vector3 direction2;
     public float velocity;
-    public bool Test;
+    public bool Test =false;
     float timer;
     int waitingTime;
-
+    // int count = 1;
 
 
     public float movePower = 5f;
-    int movementFlag = 2; // 0:Idle, 1:left, 2:right
+    int movementFlag = 1; // 0:Idle, 1:left, 2:right
 
     // Start is called before the first frame update
     void Start()
     {
         timer = 0.0f;
         waitingTime = 2;
-        StartCoroutine("ChangeMovement");
+        // StartCoroutine("ChangeMovement");
         chrRenderer = GetComponent<SpriteRenderer>();
     }
 
@@ -38,7 +40,7 @@ public class pie : MonoBehaviour
 
         target = GameObject.Find("Camolime").transform;
         // Player의 위치와 이 객체의 위치를 빼고 단위 벡터화 한다.
-        direction2 = (target.position - transform.position).normalized;
+        direction = (target.position - transform.position).normalized;
         velocity = 0.2f; /*(velocity + accelaration * Time.deltaTime);*/
         // Player와 객체 간의 거리 계산
         float distance = Vector3.Distance(target.position, transform.position);
@@ -47,17 +49,19 @@ public class pie : MonoBehaviour
 
         if (Test && !PlayerMove.instance.IsHiding)
         {
-            this.transform.position = new Vector3(transform.position.x + (direction2.x * velocity),
+            this.transform.position = new Vector3(transform.position.x + (direction.x * velocity),
                                                   transform.position.y,
-                                                  transform.position.z + (direction2.z * velocity)
+                                                  transform.position.z + (direction.z * velocity)
                                                   );
             Debug.Log("if 들어옴");
+            Debug.Log(distance);
         }
         else
         {
             Move();
 
-        }
+
+         }
 
 
         if (distance > 10.0f)
@@ -68,14 +72,14 @@ public class pie : MonoBehaviour
 
         if (distance < 5.0f)
         {
-            if(!PlayerMove.instance.IsHiding)
+            if (!PlayerMove.instance.IsHiding)
             {
-            if (timer > waitingTime)
-            {
-                timer = 0;
-                shot();
-            
-            }
+                if (timer > waitingTime)
+                {
+                    timer = 0;
+                    shot();
+
+                }
             }
         }
 
@@ -86,6 +90,8 @@ public class pie : MonoBehaviour
     {
         Vector3 moveVelocity = Vector3.zero;
 
+        Debug.Log("그냥 move 들어옴");
+
         if (movementFlag == 1)
         {
             //moveVelocity = Vector3.left;
@@ -93,17 +99,19 @@ public class pie : MonoBehaviour
 
 
             target = GameObject.Find("Flag").transform;
-            direction2 = (target.position - transform.position).normalized;
+            direction = (target.position - transform.position).normalized;
             velocity = 0.2f;
 
-            this.transform.position = new Vector3(transform.position.x + (direction2.x * velocity),
+            this.transform.position = new Vector3(transform.position.x + (direction.x * velocity),
                                       transform.position.y,
-                                      transform.position.z + (direction2.z * velocity)
+                                      transform.position.z + (direction.z * velocity)
                                       );
+
+            Debug.Log("FLAG1 들어옴");
 
             // chrRenderer.flipX = false;
 
-            this.transform.rotation = Quaternion.Euler(30, -180, 0);
+            //this.transform.rotation = Quaternion.Euler(30, -180, 0);
         }
         else if (movementFlag == 2)
         {
@@ -111,17 +119,17 @@ public class pie : MonoBehaviour
             //transform.localScale = new Vector3(-3.5f, 3.5f, 3.5f);
 
             target = GameObject.Find("Flag2").transform;
-            direction2 = (target.position - transform.position).normalized;
+            direction = (target.position - transform.position).normalized;
             velocity = 0.2f;
 
-            this.transform.position = new Vector3(transform.position.x + (direction2.x * velocity),
+            this.transform.position = new Vector3(transform.position.x + (direction.x * velocity),
                                       transform.position.y,
-                                      transform.position.z + (direction2.z * velocity)
+                                      transform.position.z + (direction.z * velocity)
                                       );
 
-            chrRenderer.flipX = true;
+            //chrRenderer.flipX = true;
 
-            this.transform.rotation = Quaternion.Euler(-30, 0, 0);
+            //this.transform.rotation = Quaternion.Euler(-30, 0, 0);
         }
         transform.position += moveVelocity * movePower * Time.deltaTime;
 
@@ -140,38 +148,67 @@ public class pie : MonoBehaviour
 
     void OnTriggerEnter(Collider col)
     {
+        if (col.transform.tag == "Flag2")
+        {
+            movementFlag = 1;
+            Debug.Log("충돌 들어옴");
 
+        }
+
+
+        if (col.transform.tag == "Flag")
+        {
+            movementFlag = 2;
+            Debug.Log("플래그 충돌");
+        }
 
     }
 
-    void OnTriggerStay(Collider col) {
-
-        Test = true;
-
-
-    }
-
-    void OnTriggerExit(Collider col) {
-
-
-    }
-
-    IEnumerator ChangeMovement()
+    void OnTriggerStay(Collider col)
     {
-        if(movementFlag==1)
-        {
-            movementFlag =2;
-        }
-        else if(movementFlag==2)
-        {
-             movementFlag =1;
-        }
-        /*movementFlag =1;*/ /*Random.Range(0, 3);*/
 
-        yield return new WaitForSeconds(10f);
+        if (col.transform.tag == "Player")
+            Test = true;
 
-        StartCoroutine("ChangeMovement");
+        if (col.transform.tag == "Flag2")
+        {
+            movementFlag = 1;
+            Debug.Log("충돌 들어옴");
+
+        }
+
+        if (col.transform.tag == "Flag")
+        {
+            movementFlag = 2;
+            Debug.Log("플래그 충돌");
+        }
+
     }
+
+    //void OnTriggerExit(Collider col)
+    //{
+
+
+    //}
+
+    //IEnumerator ChangeMovement()
+    //{
+    //    if (movementFlag == 1)
+    //    {
+    //        movementFlag = 2;
+    //    }
+    //    else if (movementFlag == 2)
+    //    {
+    //        movementFlag = 1;
+    //    }
+    //    /*movementFlag =1;*/ /*Random.Range(0, 3);*/
+
+    //    yield return new WaitForSeconds(5f);
+
+    //    StartCoroutine("ChangeMovement");
+
+    //    count += 1;
+    //}
 
 
 }
